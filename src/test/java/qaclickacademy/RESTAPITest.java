24cbaa49f2e4d5798af1620948dfce86110a60a2
kavitha79 <kavitha.reddy05@gmail.com>
2019-10-07 16:25:43 -0400

    i am new

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -8,6 +8,10 @@ public class RESTAPITest {
 	public void postJira()
 	{
 		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		
+		
 	}
 	
 	@Test
